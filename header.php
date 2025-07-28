bd84c8e022a5ed624e6a82fe5151f1127853d9ff
Nurina Maizurah <nurinamaizurah325@gmail.com>
2025-07-28 09:37:52 +0800

    ubah logo

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <body>
   <header>
     <div class="logo-box">
-      <img src="images/QaidLogo.png" alt="Qaid Travel Logo" width="250" height="250" />
+      <img src="images/QaidTravel.png" alt="Qaid Travel Logo" width="250" height="100" />
       <div class="welcome-box">
         <h1>Selamat Datang ke Qaid Travel</h1>
         <p>Ingat Melancong, Ingat Qaid Travel. "Keperluan Anda Kami Sempurnakan."</p>
