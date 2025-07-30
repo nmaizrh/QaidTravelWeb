564afefec6137e661ad3650df6ea4f4d045860f5
Nurina Maizurah <nurinamaizurah325@gmail.com>
2025-07-30 14:51:20 +0800

    update

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 
 $servername = "localhost:3301"; // Usually 'localhost' if your PHP and MySQL are on the same server
 $username = "root";        // Your MySQL username (often 'root' for development)
-$password = "";            // Your MySQL password (empty by default for XAMPP/WAMP root)
+$password = "1234";            // Your MySQL password (empty by default for XAMPP/WAMP root)
 $dbname = "qaidtravel"; // The name of the database you will create in phpMyAdmin
 
 // Create connection
@@ -13,10 +13,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Optional: Set character set to UTF-8 for proper display of special characters
-$conn->set_charset("utf8mb4");
-
-// You can now include this file in other PHP files that need database access.
-// Example: include 'db_connect.php';
 ?>
